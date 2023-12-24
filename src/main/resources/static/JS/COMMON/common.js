8ad0771efa0908dc84a221fb7420bc2509ba37d9
[user name] <invrs@naver.com>
2023-12-24 21:43:16 +0900

    엑셀다운로드 추가

--- src/main/resources/static/JS/COMMON/common.js
+++ src/main/resources/static/JS/COMMON/common.js
@@ -65,11 +65,80 @@ function dateChek(date){
 /**
 	 * 문자열이 빈 문자열인지 체크하여 결과값을 리턴한다. 
 	 * @param str		: 체크할 문자열
-	*/
+ **/
 function isEmpty(str){
 		
   if(typeof str == "undefined" || str == null || str == "")
     return true;
   else
     return false ;
+}
+
+function toDay(){
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = String(today.getMonth() + 1).padStart(2, '0');
+  const day = String(today.getDate()).padStart(2, '0');
+
+  const formattedDate = `${year}-${month}-${day}`;
+  return formattedDate;
+}
+
+
+
+/**
+	 * 엑셀 다운로드 
+	 * @param str		: 체크할 문자열
+ **/
+document.addEventListener('DOMContentLoaded', () => {
+  const excelDownload = document.querySelector('#excelDownload');
+  fileName = "거래내역";
+  excelDownload.addEventListener('click', exportExcel);
+});
+
+function exportExcel(fileName, table_id) {
+  const wb = XLSX.utils.book_new();
+  const newWorksheet = excelHandler.getWorksheet(table_id);
+  XLSX.utils.book_append_sheet(wb, newWorksheet, excelHandler.getSheetName());
+
+  const wbout = XLSX.write(wb, { bookType: 'xlsx', type: 'binary' });
+  saveAs(new Blob([s2ab(wbout)], { type: 'application/octet-stream' }), excelHandler.getExcelFileName(fileName));
+}
+
+const excelHandler = {
+  getExcelFileName: function (fileName) {
+    return fileName+toDay()+'.xlsx'; // 파일명
+  },
+  getSheetName: function () {
+    return 'Table Test Sheet'; // 시트명
+  },
+  getExcelData: function (table_id) {
+    const table = document.getElementById(table_id);
+    const rows = table.querySelectorAll('tbody tr');
+    const headerRow = table.querySelectorAll('thead th');
+    const data = [Array.from(headerRow).map(th => th.textContent.trim())];
+
+    rows.forEach(row => {
+      const rowData = [];
+      const cells = row.querySelectorAll('td input');
+
+      cells.forEach(cell => {
+        rowData.push(cell.value);
+      });
+
+      data.push(rowData);
+    });
+
+    return data;
+  },
+  getWorksheet: function (table_id) {
+    return XLSX.utils.aoa_to_sheet(this.getExcelData(table_id));
+  }
+};
+
+function s2ab(s) {
+  const buf = new ArrayBuffer(s.length); // s를 arrayBuffer로 변환
+  const view = new Uint8Array(buf); // uint8array 생성
+  for (let i = 0; i < s.length; i++) view[i] = s.charCodeAt(i) & 0xFF; // 옥텟으로 변환
+  return buf;
 }
